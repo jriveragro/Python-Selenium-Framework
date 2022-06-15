19662e04e0cd6cda192de6f1e3211dc118cb9657
Jesus Rivera <jrivera.gro@gmail.com>
2022-06-14 23:16:34 -0400

    Added get page title method.

--- pages/welcome_page.py
+++ pages/welcome_page.py
@@ -4,3 +4,8 @@ class WelcomePage:
     def __init__(self, driver, wait):
         self.driver = driver
         self.wait = wait
+
+    def get_page_title(self):
+        return self.driver.title
+    
+
