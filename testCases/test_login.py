badd4e3d6e34d9229a39bd6bdc848ade9fd316c1
Jesus Rivera <jrivera.gro@gmail.com>
2022-07-03 11:27:55 -0400

    Moved page object creation into own class in test file.

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -5,8 +5,12 @@ from utilities.utils import Utils
 
 @pytest.mark.usefixtures('setup')
 class TestLogin:
+
+    @pytest.fixture(autouse=True)
+    def class_setup(self):
+        self.wp = WelcomePage(self.driver)
+        self.ut = Utils()
+
     def test_successful_login(self):
-        wp = WelcomePage(self.driver)
-        account_overview_page = wp.log_in_existing_user("eramberxiomara357", "hola@741")
-        ut = Utils()
-        ut.assert_page_url_ends_with(account_overview_page.get_page_url(), "/parabank/overview.htm")
+        account_overview_page = self.wp.log_in_existing_user("eramberxiomara357", "hola@741")
+        self.ut.assert_page_url_ends_with(account_overview_page.get_page_url(), "/parabank/overview.htm")
--- testCases/test_register.py
+++ testCases/test_register.py
@@ -5,12 +5,13 @@ from utilities.utils import Utils
 
 @pytest.mark.usefixtures('setup')
 class TestRegister:
+
+    @pytest.fixture(autouse=True)
+    def class_setup(self):
+        self.wp = WelcomePage(self.driver)
+        self.ut = Utils()
+
     def test_register(self):
-        wp = WelcomePage(self.driver)
-        register_page = wp.click_register_link()
-        customer_created_page = register_page.register_new_user("Eramber", "Xiomara",
-                                                                "50480 Amber Dr", "Miami", "FL", "33022",
-                                                                "2003001001", "100101001",
-                                                                "eramberxiomara357", "hola@741")
-        ut = Utils()
-        ut.assert_page_url_ends_with(customer_created_page.get_page_url(), "/parabank/register.htm")
+        register_page = self.wp.click_register_link()
+        customer_created_page = register_page.register_new_user("Eramber", "Xiomara", "50480 Amber Dr", "Miami", "FL", "33022", "2003001001", "100101001", "eramberxiomara357", "hola@741")
+        self.ut.assert_page_url_ends_with(customer_created_page.get_page_url(), "/parabank/register.htm")
